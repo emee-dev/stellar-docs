b824cef03fb0fe6f97ddc842af71eb7f52d6b779
Ajike Emmanuel Jonah <emmanuelajike2000@gmail.com>
2024-08-17 18:15:25 +0100

    added the pages and docs structure

---
+++ sample.ts
@@ -0,0 +1,67 @@
+/* 
+
+Stellar by Example
+
+
+--Basic
+hello world
+first app
+Primitive Data types
+Variables
+Constants
+Immutable
+Reading and Writing to a state variable
+XLM and token Units
+Gas and Gas Price
+if / Else
+Array
+Enum
+Structs
+----Data Locations
+	instance
+	persistent
+	temporary
+function
+View and Write functions
+Error
+Function returns 
+Events
+Contructor
+Calling other contracts
+Visibility
+Macros
+Sending XLM - (token tranfer)
+----Applications
+	Freigter wallet
+	Okashi
+	Time Lock
+	Crowd Fund
+----Tests
+----Deployment
+	testnet
+	
+----Hacks
+	Re-Entrancy
+	Accessing Private Data
+	Denial of Service
+	Honeypot
+	Signature Replay
+	Vault Inflation Attack
+----Dapp
+	Stellar Js SDK
+	init.js script
+	Next js template
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
